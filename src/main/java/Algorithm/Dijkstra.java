b07c9035514811982524685e90661d5cdd43f3c6
John-Zoscak-02 <jzoscak02@gmail.com>
2021-08-13 17:57:27 -0400

    Cleaned up printing

--- src/main/java/Algorithm/Dijkstra.java
+++ src/main/java/Algorithm/Dijkstra.java
@@ -18,27 +18,6 @@ public class Dijkstra {
                 else if (o2.getVertex().isBase()) {
                     return 1;
                 }
-//                boolean o1IsNeighbor = false;
-//                boolean o2IsNeighbor = false;
-//                for ( Entry entry : getVisited() ) {
-//                    if (entry.getVertex().hasNeighbor(o1.getVertex())) {
-//                        o1IsNeighbor = true;
-//                    }
-//                    if (entry.getVertex().hasNeighbor(o2.getVertex())) {
-//                        o2IsNeighbor = true;
-//                    }
-//                }
-//
-//                if (o1IsNeighbor) {
-//                    if (o2IsNeighbor) {
-//                        return o2.getDistanceFromBase() - o1.getDistanceFromBase();
-//                    }
-//                    return -1;
-//                }
-//                if (o2IsNeighbor) {
-//                    return 1;
-//                }
-//                return o2.getDistanceFromBase() - o1.getDistanceFromBase();
                 if (o1.getPreviousNode() != null) {
                     if (o2.getPreviousNode() != null) {
                         return o1.getDistanceFromBase() - o2.getDistanceFromBase();
@@ -58,13 +37,8 @@ public class Dijkstra {
         }
     }
 
-    public List<Entry> getVisited() {
-        return visited;
-    }
-
     public void run () {
         while (!queue.isEmpty()) {
-//            System.out.println(queue);
             Entry current = queue.peek();
             Node node = current.getVertex();
             if (node.isBase()) {
@@ -75,9 +49,7 @@ public class Dijkstra {
                 }
             }
             else {
-
                 for (Edge edge : node.getEdges()) {
-//                    System.out.println("fail?");
                     if (edge.getDistance() + current.getDistanceFromBase() <= tab.get(edge.getSecond()).getDistanceFromBase()) {
                         tab.get(edge.getSecond()).setPreviousNode(node);
                         tab.get(edge.getSecond()).setDistanceFromBase(edge.getDistance() + current.getDistanceFromBase());
@@ -90,10 +62,19 @@ public class Dijkstra {
     }
 
     public void printTable() {
-        System.out.println("Vertex  Distance  Previous Vertex");
-        Collections.sort(visited);
+        System.out.println(String.format("%-20s", "VERTEX") +
+                String.format("%-20s", "DISTANCE") +
+                String.format("%-20s", "PREVIOUS VERTEX"));
+        Collections.sort(visited, new Comparator<Entry>() {
+            @Override
+            public int compare(Entry o1, Entry o2) {
+                return o1.getDistanceFromBase() - o2.getDistanceFromBase();
+            }
+        });
         for (Entry entry : visited) {
-            System.out.println(entry);
+            System.out.println(String.format("%-20s", entry.getVertex()) +
+                    String.format("%-20s", entry.getDistanceFromBase()) +
+                    String.format("%-20s", entry.getPreviousNode()) );
         }
     }
 
